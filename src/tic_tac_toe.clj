e1044e950d75f16d4e3900f4553b02ac19c1dae8
Fabio Elizandro <fabio@elizandro.com.br>
2021-12-30 18:52:09 +1100

    refactor(tic-tac-toe): convert game state to a set

--- src/tic_tac_toe.clj
+++ src/tic_tac_toe.clj
@@ -1,14 +1,22 @@
 (ns tic-tac-toe
   (:require [clojure.string :as str]))
 
+(defn new-game []
+  (hash-map "X" #{} "O" #{}))
+
 (defn player-x-move [game cell-number]
-  (assoc game cell-number "X"))
+  (assoc game "X" (conj (game "X") cell-number)))
 
 (defn player-o-move [game cell-number]
-  (assoc game cell-number "O"))
+  (assoc game "O" (conj (game "O") cell-number)))
 
 (defn- display-cell [game cell-number]
-  (str " " (or (get game cell-number) (str cell-number)) " "))
+  (str " "
+       (cond
+         (contains? (game "X") cell-number) "X"
+         (contains? (game "O") cell-number) "O"
+         :else                              (str cell-number))
+       " "))
 
 (defn display-game [game]
   (str "+---+---+---+\n"
@@ -18,12 +26,6 @@
        "+---+---+---+\n"
        "|" (str/join "|" (map #(display-cell game %) (range 7 10))) "|\n"
        "+---+---+---+"))
-
-(defn player-moves [game]
-  (reduce
-   (fn [acc [cell player]]
-     (assoc acc player (conj (acc player) cell)))
-   {"X" #{} "O" #{}} game))
 
 (def ^:private winner-sets
   #{; Horizontal sets
@@ -56,7 +58,7 @@
     :else       (player-o-move game cell-number)))
 
 (defn- run [game move]
-  (case (game-result (player-moves game))
+  (case (game-result game)
     "T"         (println "Tie.")
     "X"         (println "Player X wins.")
     "O"         (println "Player O wins.")
@@ -67,4 +69,4 @@
                   (run (run-player-input game (Integer/parseInt (read-line)) move) (+ move 1)))))
 
 (defn -main [args]
-  (run {} 1))
+  (run (new-game) 1))
--- test/tic_tac_toe_test.clj
+++ test/tic_tac_toe_test.clj
@@ -3,20 +3,14 @@
             [tic-tac-toe :refer :all]))
 
 (deftest change-game-state-for-player-X
-  (is (= {9 "X"} (player-x-move {} 9)))
-  (is (= {8 "X"} (player-x-move {} 8)))
-  (is (= {7 "X"} (player-x-move {} 7))))
+  (is (= {"X" #{9} "O" #{}} (player-x-move (new-game) 9)))
+  (is (= {"X" #{8} "O" #{}} (player-x-move (new-game) 8)))
+  (is (= {"X" #{7} "O" #{}} (player-x-move (new-game) 7))))
 
 (deftest change-game-state-for-player-O
-  (is (= {9 "O"} (player-o-move {} 9)))
-  (is (= {8 "O"} (player-o-move {} 8)))
-  (is (= {7 "O"} (player-o-move {} 7))))
-
-(deftest converts-game-state-to-player-set
-  (is (= {"X" #{1 2 3} "O" #{}} (player-moves {1 "X" 2 "X" 3 "X"})))
-  (is (= {"X" #{3 5 9} "O" #{}} (player-moves {3 "X" 5 "X" 9 "X"})))
-  (is (= {"X" #{} "O" #{1 2 3}} (player-moves {1 "O" 2 "O" 3 "O"})))
-  (is (= {"X" #{2 4} "O" #{1 3}} (player-moves {1 "O" 2 "X" 3 "O" 4 "X"}))))
+  (is (= {"X" #{} "O" #{9}} (player-o-move (new-game) 9)))
+  (is (= {"X" #{} "O" #{8}} (player-o-move (new-game) 8)))
+  (is (= {"X" #{} "O" #{7}} (player-o-move (new-game) 7))))
 
 (deftest returns-game-winner-for-horizontal-matches
   (is (= "X" (game-result {"X" #{1 2 3} "O" #{}})))
@@ -57,7 +51,7 @@
 +---+---+---+
 | 7 | 8 | 9 |
 +---+---+---+"
-      (display-game {})))
+      (display-game (new-game))))
   (is
    (= "+---+---+---+
 | 1 | 2 | 3 |
@@ -66,7 +60,7 @@
 +---+---+---+
 | 7 | 8 | X |
 +---+---+---+"
-      (display-game (player-x-move {} 9))))
+      (display-game (player-x-move (new-game) 9))))
   (is
    (= "+---+---+---+
 | X | 2 | O |
@@ -75,9 +69,5 @@
 +---+---+---+
 | 7 | 8 | X |
 +---+---+---+"
-      (display-game
-       {1 "X"
-        5 "X"
-        9 "X"
-        3 "O"
-        6 "O"}))))
+      (display-game {"X" #{1 5 9} "O" #{3 6}}))))
+
