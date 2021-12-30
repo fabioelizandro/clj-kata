195e99077c0d05905ea1099123a47e10dcbd49c9
Fabio Elizandro <fabio@elizandro.com.br>
2021-12-30 13:27:35 +1100

    chore(tic-tac-toe): format files

--- src/tic_tac_toe.clj
+++ src/tic_tac_toe.clj
@@ -8,15 +8,15 @@
   (assoc game cell-number "O"))
 
 (defn- display-cell [game cell-number]
-  (str  " " (or (get game cell-number) (str cell-number)) " "))
+  (str " " (or (get game cell-number) (str cell-number)) " "))
 
 (defn display-game [game]
   (str "+---+---+---+\n"
-       "|"(str/join "|" (map #(display-cell game %) (range 1 4)))"|\n"
+       "|" (str/join "|" (map #(display-cell game %) (range 1 4))) "|\n"
        "+---+---+---+\n"
-       "|"(str/join "|" (map #(display-cell game %) (range 4 7)))"|\n"
+       "|" (str/join "|" (map #(display-cell game %) (range 4 7))) "|\n"
        "+---+---+---+\n"
-       "|"(str/join "|" (map #(display-cell game %) (range 7 10)))"|\n"
+       "|" (str/join "|" (map #(display-cell game %) (range 7 10))) "|\n"
        "+---+---+---+"))
 
 (defn player-moves [game]
@@ -28,21 +28,21 @@
 (defn- run-player-read-line-question [move]
   (cond
     (odd? move) "Player X:"
-    :else "Player O:"))
+    :else       "Player O:"))
 
 (defn- run-player-input [game cell-number move]
   (cond
     (odd? move) (player-x-move game cell-number)
-    :else (player-o-move game cell-number)))
+    :else       (player-o-move game cell-number)))
 
 (defn- run [game move]
   (cond
     (= move 10) (println "end of game")
-    :else (do
-            (println (display-game game))
-            (println (run-player-read-line-question move))
-            (flush)
-            (run (run-player-input game (Integer/parseInt (read-line)) move) (+ move 1)))))
+    :else       (do
+                  (println (display-game game))
+                  (println (run-player-read-line-question move))
+                  (flush)
+                  (run (run-player-input game (Integer/parseInt (read-line)) move) (+ move 1)))))
 
 (defn -main [args]
   (run {} 1))
